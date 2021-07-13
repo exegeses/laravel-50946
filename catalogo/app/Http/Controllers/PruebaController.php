065bec15ede24d681e6cf87207be3f11bcc0cc3f
Marcos <exegeses@gmail.com>
2021-07-13 20:25:14 -0300

    Panel de administración de marcas

---
+++ catalogo/app/Http/Controllers/PruebaController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PruebaController extends Controller
+{
+    //método  index()
+    public function listarRecurso()
+    {
+    }
+    //métodos  create() y store()
+    public function agregarRecurso()
+    {
+    }
+    //método  show()
+    public function verRecursoPorID()
+    {
+    }
+    //métodos edit() y update()
+    public function modificarRecurso()
+    {
+    }
+    //método  destroy()
+    public function eliminarRecurso()
+    {
+    }
+}
